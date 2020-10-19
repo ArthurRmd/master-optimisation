ed438e1a289b37f8167f2666fb07b51dd7a23bea
ArthurRmd <arthur.remond@protonmail.com>
2020-10-19 23:22:56 +0200

    update : use two thread ⚡️

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/buptmiao/parallel"
 	"math/rand"
 	"time"
 )
@@ -13,10 +14,44 @@ func main() {
 	rand.Seed(2)
 
 	binaries := generateBinaries()
+	binaries2 := generateBinaries()
+	//binaries3 := generateBinaries()
 
 	var knapsack Knapsack = getKnapsackByFile("data.txt")
-	response := getRandom(100000, knapsack, binaries)
-	fmt.Println(response.profit)
+	var knapsack2 Knapsack = getKnapsackByFile("data.txt")
+	//var knapsack3 Knapsack = getKnapsackByFile("data.txt")
+
+	var response1 KnapsackResponse
+	var response2 KnapsackResponse
+	var response3 KnapsackResponse
+
+	//	response := getRandom(50000, knapsack, binaries)
+	//	response2 := getRandom(50000, knapsack2, binaries2)
+
+	numberPerThread := 350000
+	p := parallel.NewParallel()
+
+	p.Register(getRandom, numberPerThread, knapsack, binaries).SetReceivers(&response1)
+	p.Register(getRandom, numberPerThread, knapsack2, binaries2).SetReceivers(&response2)
+	//p.Register(getRandom, numberPerThread, knapsack3, binaries3).SetReceivers(&response3)
+
+	p.Run()
+
+	bestProfit := 0
+
+	if response1.profit> response2.profit {
+		bestProfit = response1.profit
+	} else {
+		bestProfit = response2.profit
+	}
+
+	if bestProfit < response3.profit {
+		bestProfit = response3.profit
+	}
+
+	fmt.Println(bestProfit)
+
+
 
 	elapsed := time.Since(start)
 	fmt.Println(elapsed)
